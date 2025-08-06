ac09388fd25597d94143a83ce21cb8201afe1017
manju <manju@PushpaManjunath>
2025-08-06 14:27:40 +0200

    thanks has been replied

--- src/test/java/GitHubInAutomation/LearningGitHubInAutomationTesting.java
+++ src/test/java/GitHubInAutomation/LearningGitHubInAutomationTesting.java
@@ -5,6 +5,7 @@ public class LearningGitHubInAutomationTesting {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Avance Selenium");
+		System.out.println("Thanks a lot");
 
 	}
 
